d19ae411e913674011d7c0212f913ea1b0eacac3
dwillhelm <dwillhelm@enthought.com>
2023-07-20 10:20:09 -0500

    init build dir

---
+++ streamlit_cropper/frontend/build/precache-manifest.c93850bde9c65e7940dc021538ecfb5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5eb94a64d74d5eece4bef718ba4134",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f9092613e615ce069b55",
+    "url": "./static/js/2.7d0b7ccf.chunk.js"
+  },
+  {
+    "revision": "a120daea67c98f2907b7f8aedcea87dd",
+    "url": "./static/js/2.7d0b7ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67271d765af0e7a6b5f7",
+    "url": "./static/js/main.3e6639db.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.3e6639db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
